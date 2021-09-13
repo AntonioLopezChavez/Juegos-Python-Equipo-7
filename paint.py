6c49a22696bff89db80acdf76c8c057357ba2a98
Hugo Palomares <80979871+occon@users.noreply.github.com>
2021-09-13 16:21:32 -0600

    [Feature] Added triangle and rectangle shapes

--- paint.py
+++ paint.py
@@ -10,9 +10,9 @@ Exercises
 
 """
 
-from turtle import *    # Importar módulo turtle
+from turtle import *
 
-from freegames import vector    # Importar módulo freegames y vectores para descargar videojuegos de python gratis
+from freegames import vector
 
 
 def line(start, end):
@@ -44,12 +44,33 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x - start.x)/2)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
-    "Draw triangle from start to end."
-    pass  # TODO
+    "Draw triangle from start to end." 
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 
 def tap(x, y):
