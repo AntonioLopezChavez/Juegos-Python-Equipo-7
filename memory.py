5330cb958d2488132d30d3d7269a7950a7ba52d0
Hugo Palomares <80979871+occon@users.noreply.github.com>
2021-09-15 12:56:35 -0600

    Revert "cs"
    This reverts commit d3ae8e4f420fe64bd85c02118991efb23f49412d.

--- memory.py
+++ memory.py
@@ -1,15 +1,11 @@
 from random import *
 from turtle import *
-
 from freegames import path
 
 car = path('car.gif')
-rubb = ("python","c++","html","css","java","flutter","c","cpp","py","django","db","cs","code","pro","stress","macos","windows","linux","data","science","msft","google","fb","intern","yahoo","linkedin","binary","sort","merge","bigO","$","bug","python","c++","html","css","java","flutter","c","cpp","py","django","db","cs","code","pro","stress","macos","windows","linux","data","science","msft","google","fb","intern","yahoo","linkedin","binary","sort","merge","bigO","$","bug")
-tiles = list(rubb)
-print(tiles)
+tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-counter = 0
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -23,20 +19,16 @@ def square(x, y):
         left(90)
     end_fill()
 
-
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
 
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
-
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
-    #counter += 1
     spot = index(x, y)
     mark = state['mark']
 
@@ -46,7 +38,6 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
-
 
 def draw():
     "Draw image and tiles."
@@ -65,14 +56,13 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 25, y+18) #########
+        goto(x + 2, y)
         color('black')
-        write(tiles[mark], font=('Arial', 10, 'normal'))
+        write(tiles[mark], font=('Arial', 30, 'normal'))
 
     update()
-    ontimer(draw(), 100)
+    ontimer(draw, 100)
 
-counter = 0
 shuffle(tiles)
 setup(420, 420, 370, 0)
 addshape(car)
